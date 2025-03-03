57497585b989973344742f338d40b0ca4e1c3b09
Zamba Izo <ofuzak@gmail.com>
2025-03-03 08:17:49 -0500

    Add Google Tag  Manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,32 +1,71 @@
 <!DOCTYPE html>
 <html class="dark" dark lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="manifest" href="/site.webmanifest" />
 
-        <!-- Scripts -->
-        @routes
-        @if(request()->is('install*'))
-            @vite(['resources/js/install.js', "resources/js/Pages/{$page['component']}.vue"])
-        @else
-             @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @endif
-        @inertiaHead
-        <script src="https://s3.tradingview.com/tv.js"></script>
-        
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @if (request()->is('install*'))
+        @vite(['resources/js/install.js', "resources/js/Pages/{$page['component']}.vue"])
+    @else
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @endif
+    @inertiaHead
+    <script src="https://s3.tradingview.com/tv.js"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JJ5HZEMSX"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-2JJ5HZEMSX');
+    </script>
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PMB4NTL5');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <meta name="google-site-verification" content="-scA-CEWzvZpnp-CmWmt9fo_vu0XLX67S1qvrRwUG40" />
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMB4NTL5" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+</body>
+
 </html>
