20cc47114ea8dd5ffd6ba27a4ade630a801cb0ca
Scriptoshi <scriptoshi.com@gmail.com>
2025-06-01 05:29:06 -0400

    Fx drops

--- resources/js/Pages/Nfts/Mint.vue
+++ resources/js/Pages/Nfts/Mint.vue
@@ -23,6 +23,19 @@ const chainId = useChainId();
 
     <Head title="KYC NFT Minting" />
     <AppLayout>
+
+        <div class=" text-white mb-8 max-w-3xl">
+            <div class="text-2xl font-bold mb-4">
+                KYC NFT Minting
+            </div>
+            <h3 class="text-lg font-semibold mb-2">SHARK TOKEN 2025 AIRDROP</h3>
+            SHARK TOKEN NFTS will be airdropped to users. SHARK represent the share of platform fees you can
+            collect. All fees collected by the platform will be distributed to members.
+            To collect fees, you will need to burn your share of sharktokens. This mechanism will ensure that token is
+            deflationary over time.
+            <br />
+            To recieve the airdrop, ensure to collect all our NFTS below.
+        </div>
         <NftMint
             v-if="kycNfts && kycNfts[chainId]"
             :balances="balances"
